0e7b125618f5703bc6e931b2f9fab2eaac4b8bc5
github username <jaycru06@yahoo.com>
2021-01-14 18:52:33 -0500

    commit tree afc92067977e9d667e781b10f5c66dd1a15ba49a

--- index.js
+++ index.js
@@ -6,26 +6,41 @@ class Circle {
    }
 
    get diameter() {
-      
+      // Diameter = radius • 2
+      return this.radius * 2;
    }
 
    get circumference() {
-
+      // Circumference = π • diameter
+      return Math.PI * this.diameter
    }
 
    get area() {
-
+      // Area = π • radius2
+      return Math.PI * (this.radius ** 2)
    }
 
-   set diameter() {
-      
+   set diameter(newDiameter) {
+      if (newDiameter > 0) {
+         this.radius = newDiameter / 2;
+      } else {
+         throw new Error("Diameter cannot be less than 0");
+      }
    }
 
-   set circumference() {
-
+   set circumference(newCircumference) {
+      if (newCircumference > 0) {
+         this.radius = (newCircumference / 2) / Math.PI;
+      } else {
+         throw new Error("Circumference cannot be less than 0");
+      }
    }
 
-   set area() {
-      
+   set area(newArea) {
+      if (newArea> 0) {
+         this.radius = Math.sqrt(newArea);
+      } else {
+         throw new Error("Area cannot be less than 0");
+      }
    }
 }
